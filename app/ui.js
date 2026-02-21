93dbf8abbf2b1a66d2f63c4acc786112ba82a5b0
officialiambradley <officialiambradley@gmail.com>
2026-02-21 17:59:57 -0400

    Creating app

---
+++ app/ui.js
@@ -0,0 +1,9 @@
+export function SurveyCard() {
+    return (
+        <div>
+            Healthcare Prescriptions Research
+
+            <button className="bg-[#20BCDF] text-[16px] leading-[16px] tracking-[-1px] p-4 px-8 rounded-[5px] font-bold text-white">Begin</button>
+        </div>
+    );
+}
